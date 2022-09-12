ecea677be504213f22bf06f9082544f708011218
Aliaksei Kozel <Aliaksei_Kozel@ultimatesoftware.com>
2022-09-12 22:08:09 +0400

    initial commit

---
+++ src/main/java/by/akozel/springstreaming/SpringStreamingApplication.java
@@ -0,0 +1,13 @@
+package by.akozel.springstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStreamingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringStreamingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/by/akozel/springstreaming/SpringStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package by.akozel.springstreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStreamingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
